4f499ebdc8e9c57493df674715b0c147a38fe0b2
Adi Avishalom <avishalomadi@gmail.com>
2019-03-06 12:33:10 -0500

    Updated Coin Flip to reset per flip

--- app/src/main/java/com/adiavishalom/adismtglifecounter/FlipCoin.java
+++ app/src/main/java/com/adiavishalom/adismtglifecounter/FlipCoin.java
@@ -14,6 +14,7 @@ public class FlipCoin extends AppCompatActivity {
     private TextView result;
     private Button flip;
     private Random rng = new Random();
+    private boolean flipped = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,8 @@ public class FlipCoin extends AppCompatActivity {
         coin = findViewById(R.id.coinView);
         result = findViewById(R.id.result);
         flip = findViewById(R.id.flip);
+        coin.setVisibility(View.INVISIBLE);
+
 
 
         flip.setOnClickListener(new View.OnClickListener() {
@@ -30,22 +33,35 @@ public class FlipCoin extends AppCompatActivity {
             public void onClick(View v) {
                 int randomNumber = rng.nextInt(2) + 1;
                 flipCoin(randomNumber);
-
             }
         });
     }
     private void flipCoin(int randomNumber)
-    {
-        if (randomNumber == 1)
-        {
-            coin.setImageResource(R.drawable.coin_head);
-            result.setText("HEADS!");
-        }
-        else
-        {
-            coin.setImageResource(R.drawable.coin_tail);
-            result.setText("TAILS!");
-        }
+    {                                                                   //////////////////////////////////////////////////////////////////////
+        if(flipped)                                                     //If flipped is true, then the button is currently set to reset     //
+        {                                                               //                                                                  //
+            flip.setText("Flip Coin");                                  //Button becomes Flip Coin again                                    //
+            coin.setVisibility(View.INVISIBLE);                         //the coin image disappears                                         //
+            result.setText("");                                         //result is cleared                                                 //
+            flipped = false;                                            //flipped is now false                                              //
+        }                                                               //////////////////////////////////////////////////////////////////////
+        else                                                            //If flipped is false, then the button is currently set to Flip Coin//
+        {                                                           //                                                                  //
+            if (randomNumber == 1)                                  //if rng=1,                                                         //
+            {                                                       //                                                                  //
+                coin.setImageResource(R.drawable.coin_head);        //make image Heads                                                  //
+                result.setText("HEADS!");                           //display Heads                                                     //
+            }                                                       //                                                                  //
+            else                                                    //rng can only equal 1 or 2, so if its not 1,                       //
+            {                                                       //                                                                  //
+                coin.setImageResource(R.drawable.coin_tail);        //make image Tails                                                  //
+                result.setText("TAILS!");                           //display Tails                                                     //
+            }                                                       //                                                                  //
+            coin.setVisibility(View.VISIBLE);                       //make the coin image appear showing that the coin is flipped       //
+            flip.setText("Reset");                                  //button becomes reset                                              //
+            flipped = true;                                         //not the status of the coin is flipped                             //
+        }                                                           //////////////////////////////////////////////////////////////////////
+
 
         return;
 
